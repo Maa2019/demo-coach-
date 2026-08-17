f9ee82e9a476ac8d9b78aa38f5a15a83f9d3028a
Maa2019 <arijit.ghosh15@gmail.com>
2026-08-17 21:48:20 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,24 +1,9 @@
-const CACHE = "lloyd-demo-coach-v2";
-const ASSETS = [
-  "./", "./index.html", "./manifest.json",
-  "./img/card-32.jpg", "./img/card-43.jpg", "./img/card-55.jpg",
-  "./icons/icon-192.png", "./icons/icon-512.png"
-];
-self.addEventListener("install", e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
-});
-self.addEventListener("activate", e => {
-  e.waitUntil(caches.keys().then(ks =>
-    Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ).then(() => self.clients.claim()));
-});
-self.addEventListener("fetch", e => {
-  if (e.request.method !== "GET") return;
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request).then(resp => {
-      const copy = resp.clone();
-      caches.open(CACHE).then(c => c.put(e.request, copy)).catch(()=>{});
-      return resp;
-    }).catch(() => caches.match("./index.html")))
-  );
-});
+const CACHE="lloyd-demo-coach-easy-v1";
+self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE)
+  .then(c=>c.addAll(["./","./index.html","./manifest.json"])).then(()=>self.skipWaiting()));});
+self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(ks=>
+  Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
+self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{
+    const c2=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,c2)).catch(()=>{});
+    return resp;}).catch(()=>caches.match("./index.html"))));});
