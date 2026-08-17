974eaf8131f985b1611666ba394dc08439b92984
Maa2019 <arijit.ghosh15@gmail.com>
2026-08-17 22:42:33 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE="lloyd-demo-coach-easy-v1";
+const CACHE="lloyd-demo-coach-easy-v3";
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE)
   .then(c=>c.addAll(["./","./index.html","./manifest.json"])).then(()=>self.skipWaiting()));});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(ks=>
   Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
 self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{
-    const c2=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,c2)).catch(()=>{});
-    return resp;}).catch(()=>caches.match("./index.html"))));});
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(x=>{
+    const c2=x.clone();caches.open(CACHE).then(c=>c.put(e.request,c2)).catch(()=>{});return x;
+  }).catch(()=>caches.match("./index.html"))));});
